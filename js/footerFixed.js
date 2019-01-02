9bad7a0ac787e4452bcf81f547d2d22fe77bc559
takuyasugarapple <takuyasugarapple@gmail.com>
2019-01-02 23:59:53 +0900

    Create style.scssc
    教材の３パターンをとりあえず作成

--- js/footerFixed.js
+++ js/footerFixed.js
@@ -9,11 +9,11 @@
  *  
  *--------------------------------------------------------------------------*/
 
-new function(){
-	
+new function () {
+
 	var footerId = "footer";
 	//メイン
-	function footerFixed(){
+	function footerFixed() {
 		//ドキュメントの高さ
 		var dh = document.getElementsByTagName("body")[0].clientHeight;
 		//フッターのtopからの位置
@@ -22,53 +22,53 @@ new function(){
 		//フッターの高さ
 		var fh = document.getElementById(footerId).offsetHeight;
 		//ウィンドウの高さ
-		if (window.innerHeight){
+		if (window.innerHeight) {
 			var wh = window.innerHeight;
-		}else if(document.documentElement && document.documentElement.clientHeight != 0){
+		} else if (document.documentElement && document.documentElement.clientHeight != 0) {
 			var wh = document.documentElement.clientHeight;
 		}
-		if(ft+fh<wh){
+		if (ft + fh < wh) {
 			document.getElementById(footerId).style.position = "relative";
-			document.getElementById(footerId).style.top = (wh-fh-ft-1)+"px";
+			document.getElementById(footerId).style.top = (wh - fh - ft - 1) + "px";
 		}
 	}
-	
+
 	//文字サイズ
-	function checkFontSize(func){
-	
+	function checkFontSize(func) {
+
 		//判定要素の追加	
 		var e = document.createElement("div");
 		var s = document.createTextNode("S");
 		e.appendChild(s);
-		e.style.visibility="hidden"
-		e.style.position="absolute"
-		e.style.top="0"
+		e.style.visibility = "hidden"
+		e.style.position = "absolute"
+		e.style.top = "0"
 		document.body.appendChild(e);
 		var defHeight = e.offsetHeight;
-		
+
 		//判定関数
-		function checkBoxSize(){
-			if(defHeight != e.offsetHeight){
+		function checkBoxSize() {
+			if (defHeight != e.offsetHeight) {
 				func();
-				defHeight= e.offsetHeight;
+				defHeight = e.offsetHeight;
 			}
 		}
-		setInterval(checkBoxSize,1000)
+		setInterval(checkBoxSize, 1000)
 	}
-	
+
 	//イベントリスナー
-	function addEvent(elm,listener,fn){
-		try{
-			elm.addEventListener(listener,fn,false);
-		}catch(e){
-			elm.attachEvent("on"+listener,fn);
+	function addEvent(elm, listener, fn) {
+		try {
+			elm.addEventListener(listener, fn, false);
+		} catch (e) {
+			elm.attachEvent("on" + listener, fn);
 		}
 	}
 
-	addEvent(window,"load",footerFixed);
-	addEvent(window,"load",function(){
+	addEvent(window, "load", footerFixed);
+	addEvent(window, "load", function () {
 		checkFontSize(footerFixed);
 	});
-	addEvent(window,"resize",footerFixed);
-	
+	addEvent(window, "resize", footerFixed);
+
 }
