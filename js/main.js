27e387ad36e019f90e1f6129b6dea3347c1fb5b6
takuyasugarapple <takuyasugarapple@gmail.com>
2019-02-01 19:08:39 +0900

    Update .DS_Store
    一旦プッシュ

---
+++ js/main.js
@@ -0,0 +1,97 @@
+$(function () {
+
+    $(document).on('click', '.slides-item1', function (event) {
+        event.preventDefault();
+        $('#slides-modal1').iziModal('open');
+    });
+    $('#slides-modal1').iziModal();
+
+    $(document).on('click', '.slides-item2', function (event) {
+        event.preventDefault();
+        $('#slides-modal2').iziModal('open');
+    });
+    $('#slides-modal2').iziModal();
+
+    $(document).on('click', '.slides-item3', function (event) {
+        event.preventDefault();
+        $('#slides-modal3').iziModal('open');
+    });
+    $('#slides-modal3').iziModal();
+
+    $(document).on('click', '.slides-item4', function (event) {
+        event.preventDefault();
+        $('#slides-modal4').iziModal('open');
+    });
+    $('#slides-modal4').iziModal();
+
+    $(document).on('click', '.slides-item5', function (event) {
+        event.preventDefault();
+        $('#slides-modal5').iziModal('open');
+    });
+    $('#slides-modal5').iziModal();
+
+    $(document).on('click', '.slides-item6', function (event) {
+        event.preventDefault();
+        $('#slides-modal6').iziModal('open');
+    });
+    $('#slides-modal6').iziModal();
+
+    $(document).on('click', '.slides-item7', function (event) {
+        event.preventDefault();
+        $('#slides-modal7').iziModal('open');
+    });
+    $('#slides-modal7').iziModal();
+
+    $(document).on('click', '.slides-item8', function (event) {
+        event.preventDefault();
+        $('#slides-modal8').iziModal('open');
+    });
+    $('#slides-modal8').iziModal();
+
+    $(document).on('click', '.slides-item9', function (event) {
+        event.preventDefault();
+        $('#slides-modal9').iziModal('open');
+    });
+    $('#slides-modal9').iziModal();
+
+    $(document).on('click', '.slides-item10', function (event) {
+        event.preventDefault();
+        $('#slides-modal10').iziModal('open');
+    });
+    $('#slides-modal10').iziModal();
+
+    $(document).on('click', '.slides-item11', function (event) {
+        event.preventDefault();
+        $('#slides-modal11').iziModal('open');
+    });
+    $('#slides-modal11').iziModal();
+
+    $(document).on('click', '.slides-item12', function (event) {
+        event.preventDefault();
+        $('#slides-modal12').iziModal('open');
+    });
+    $('#slides-modal12').iziModal();
+
+    $(document).on('click', '.slides-item13', function (event) {
+        event.preventDefault();
+        $('#slides-modal13').iziModal('open');
+    });
+    $('#slides-modal13').iziModal();
+
+    $(document).on('click', '.slides-item14', function (event) {
+        event.preventDefault();
+        $('#slides-modal14').iziModal('open');
+    });
+    $('#slides-modal14').iziModal();
+
+    $(document).on('click', '.slides-item15', function (event) {
+        event.preventDefault();
+        $('#slides-modal15').iziModal('open');
+    });
+    $('#slides-modal15').iziModal();
+
+    $('.slider').slick({
+        dots: true
+    });
+
+});
