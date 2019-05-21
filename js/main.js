9c45401a19dc8c62f0b2d7f59837ec5ca3448c62
takuyasugarapple <takuyasugarapple@gmail.com>
2019-05-21 15:03:20 +0900

    Update style.min.css
    一旦コミット

--- js/main.js
+++ js/main.js
@@ -42,4 +42,37 @@ $(document).ready(function () {
         .focus(function () {
             $('.label_repass').addClass("focus")
         });
+
+
+
+    $('#input1').val(location.href);
+
+    $(function () {
+        $('.share_btn').click(function () {
+            $('#input1').focus();
+            $('#input1').select();
+
+            document.execCommand('copy');
+        });
+    });
+
+    var $lightbox = $('#lightbox');
+    $('[data-target="#lightbox"]').on('click', function (event) {
+        var $img = $(this).find('img'),
+            src = $img.attr('src'),
+            alt = $img.attr('alt');
+        $lightbox.find('img').attr('src', src);
+        $lightbox.find('img').attr('alt', alt);
+        $lightbox.find('img').css(css);
+    });
+    $lightbox.on('shown.bs.modal', function (e) {
+        var $img = $lightbox.find('img');
+        // $lightbox.find('.modal-dialog').css({
+        //     'width': $img.width()
+        // });
+        $lightbox.find('.close').removeClass('hidden');
+    });
+
+    
+
 });
